f2f32e3b78181fcbb9c8c8299c4f2d0d62c7a720
Gustavo Niemeyer <niemeyer@conectiva.com>
2003-06-10 03:07:34 +0000

    New apt-files plugin, in the contrib/ directory.

---
+++ contrib/apt-files/convert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys, os
+import rpm
+
+def main():
+    if len(sys.argv) == 1:
+        sys.exit("Usage: convert.py <hdlist file|rpms dir> ...")
+
+    for entry in sys.argv[1:]:
+        if os.path.isfile(entry):
+            for header in rpm.readHeaderListFromFile(entry):
+                name = header[rpm.RPMTAG_NAME]
+                for filename in header[rpm.RPMTAG_FILENAMES]:
+                    print filename, name
+        if os.path.isdir(entry):
+            for filename in os.listdir(entry):
+                if filename.endswith(".rpm"):
+                    filepath = os.path.join(entry, filename)
+                    file = open(filepath)
+                    header = rpm.headerFromPackage(file.fileno())[0]
+                    name = header[rpm.RPMTAG_NAME]
+                    for filename in header[rpm.RPMTAG_FILENAMES]:
+                        print filename, name
+                    file.close()
+
+if __name__ == "__main__":
+    main()
+
