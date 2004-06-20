4a2e8c4eea29000c3080f3a2306ad81195d299b7
Panu Matilainen <pmatilai@laiskiainen.org>
2004-06-20 12:15:08 +0000

    Implemented progress reporting in contrib/gpg-check.

--- contrib/gpg-check/gpg-check.lua
+++ contrib/gpg-check/gpg-check.lua
@@ -1,42 +1,77 @@
 if confget("RPM::GPG-Check/b", "true") == "false" then
-	return
+    return
 end
 
 if table.getn(files_install) < 1 then
-	return
+    return
 end
 
-print(_("Checking GPG signatures..."))
+hash = '###########################################'
+hashestotal = string.len(hash)
+interactive = confget("RPM::Interactive/b", "true")
+quiet = tonumber(confget("quiet", 0))
+
+function printhash(amount, total)
+    percent = amount/total*100
+    if interactive == "true" then
+	nrhash = hashestotal - hashestotal / total * amount
+	line = string.format("%-44s[%3d%%]", string.sub(hash, nrhash), percent)
+	io.stdout.write(io.stdout, line)
+	io.stdout.flush(io.stdout)
+	for i = 1, string.len(line) do
+	    io.stdout.write(io.stdout, '\b')
+	end
+    else
+	io.stdout.write(io.stdout, string.format("%%%% %f\n", percent))
+    end
+end
+	
+function showerrors(i, msg)
+    apterror(msg)
+end
+
 good = 1
 unknown = 0
 illegal = 0
 unsigned = 0
+errors = {}
 
+io.stdout.write(io.stdout, string.format("%-28s", _("Checking GPG signatures...")))
+if interactive == "false" then
+	io.stdout.write(io.stdout, '\n')
+end
 for i, file in ipairs(files_install) do
+    if quiet == 0 then
+	printhash(i, table.getn(files_install))
+    end
     inp = io.popen("LANG=C /bin/rpm --checksig  "..file.." 2>&1")
  
     for line in inp.lines(inp) do
         if string.find(line, "gpg") then
             break
         elseif string.find(line, "GPG") then
-            print(_("Unknown signature "..line))
+            table.insert(errors, _("Unknown signature "..line))
 			unknown = unknown + 1
             good = nil
         elseif string.find(line, "rpmReadSignature") then
-            print(_("Illegal signature "..line))
+            table.insert(errors, _("Illegal signature "..line))
 			illegal = illegal + 1
             good = nil
         else
-            print(_("Unsigned "..line))
+            table.insert(errors, _("Unsigned "..line))
 			unsigned = unsigned + 1
 			good = nil
         end
     end
     io.close(inp)
 end
-
-if not good then
-	apterror(_("Error(s) while checking package signatures:\n"..unsigned.." unsigned package(s)\n"..unknown.." package(s) with unknown signatures\n"..illegal.." package(s) with illegal/corrupted signatures"))
+if interactive == "true" then
+    io.stdout.write(io.stdout, '\n')
 end
 
--- vim::ts=4
+if not good then
+    table.foreach(errors, showerrors)
+    apterror(_("Error(s) while checking package signatures:\n"..unsigned.." unsigned package(s)\n"..unknown.." package(s) with unknown signatures\n"..illegal.." package(s) with illegal/corrupted signatures"))
+end
+
+-- vim::sts=4:sw=4
